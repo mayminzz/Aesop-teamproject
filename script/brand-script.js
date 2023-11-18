8b2b8025a46191cd19d27f4c6da9dc0864cd927f
naknak0 <naknvn77@gmail.com>
2023-11-18 21:34:08 +0900

    brandstoyr_231118

---
+++ script/brand-script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // 슬라이드
+  $(".br-slidercon").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2800,
+  });
+});
+
+// const buttons = document.querySelectorAll("a.story-btn");
+// buttons.forEach((button) =>
+//   button.addEventListener("mouseover", (e) => {
+//     button.style.color = "#8d8d8d";//버튼 태그용
+//     img.scr = "/img/brandstory/icons/arrow_gray.png"
+//   })
+//   button.addEventListener("mouseout", () => {
+//     button.style.color = "";//버튼 태그용
+//     img.scr = "/img/brandstory/icons/arrow_bk.png"
+//   })
+// );
